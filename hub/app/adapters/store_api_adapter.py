42dae034c097900a93f89a894355709eac2fd50b
Dichik <omeluan.dima@gmail.com>
2024-02-25 11:23:43 +0200

    Add implementation (assignment 3)

--- hub/app/adapters/store_api_adapter.py
+++ hub/app/adapters/store_api_adapter.py
@@ -2,15 +2,17 @@ import json
 import logging
 from typing import List
 
-import pydantic_core
 import requests
 
 from app.entities.processed_agent_data import ProcessedAgentData
 from app.interfaces.store_gateway import StoreGateway
 
+
 class StoreApiAdapter(StoreGateway):
-    def __init__(self, api_base_url):
+    def __init__(self, api_base_url, buffer_size=10):
         self.api_base_url = api_base_url
+        self.buffer_size = buffer_size
+        self.buffer = []
 
     def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]) -> bool:
         """
@@ -20,12 +22,31 @@ class StoreApiAdapter(StoreGateway):
         Returns:
             bool: True if the data is successfully saved, False otherwise.
         """
-        url = f"{self.api_base_url}/agent_data"  # Construct the API endpoint URL
         try:
-            json_data = [processed_data.dict() for processed_data in processed_agent_data_batch]
+            # Add the batch of data to the buffer
+            self.buffer.extend(processed_agent_data_batch)
 
+            # Check if the buffer size has been reached
+            if len(self.buffer) >= self.buffer_size:
+                # If buffer size is reached, send the data
+                success = self.send_data(self.buffer)
+                # Clear the buffer after sending
+                self.buffer.clear()
+                return success
+            else:
+                return True
+        except Exception as e:
+            logging.error(f"Error occurred: {e}")
+            return False
+
+    def send_data(self, data: List[ProcessedAgentData]) -> bool:
+        """
+        Send the accumulated data to the Store API.
+        """
+        url = f"{self.api_base_url}/agent_data"
+        try:
+            json_data = [processed_data.dict() for processed_data in data]
             response = requests.post(url, json=json_data)
-
             if response.status_code == 201:
                 return True
             else:
