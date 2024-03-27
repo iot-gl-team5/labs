ba247c972439364f909d25fe0a66ab82d06a271e
Oleksii Kuznietsov <Oleksii.Kuznietsov@sparrowacoustics.com>
2024-03-28 01:09:06 +0200

    feat(mobile_app): WIP update business logic
    Send data via mqtt.
    Handle system events properly.

--- mobile_app/lib/iot/metrics_controller_adapter.dart
+++ mobile_app/lib/iot/metrics_controller_adapter.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/foundation.dart';
 import 'package:logger/web.dart';
 import 'package:mobile_app/iot/accelerometer.dart';
+import 'package:mobile_app/iot/agent_data.dart';
 import 'package:mobile_app/iot/location.dart';
 import 'package:mobile_app/iot/metrics_controller.dart';
+import 'package:mobile_app/iot/mqtt_model.dart';
 import 'package:mobile_app/utils/extensions/subscription_extension.dart';
 import 'package:mobile_app/utils/subscription_manager.dart';
 import 'package:sensors_plus/sensors_plus.dart' as sensors;
 import 'package:sensors_plus/sensors_plus.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+const _userId = int.fromEnvironment(
+  'USER_ID',
+  defaultValue: 777,
+);
+const _topic = String.fromEnvironment(
+  'TOPIC',
+  defaultValue: 'processed_agent_data_topic',
+);
+const _port = int.fromEnvironment(
+  'PORT',
+  defaultValue: 1883,
+);
+
+const _host = String.fromEnvironment(
+  'HOST',
+  defaultValue: 'localhost',
+);
 
 class MetricsControllerAdapter extends MetricsController
     with SubscriptionManager {
@@ -15,20 +38,105 @@ class MetricsControllerAdapter extends MetricsController
     super.state, {
     required this.logger,
   }) {
-    sensors
-        .accelerometerEventStream(samplingPeriod: SensorInterval.gameInterval)
-        .distinct()
-        .map(_mapToAccelerometerState)
-        .listen(_handleAccelerometerData, onError: _handleError)
-        .addToList(this);
-
     Geolocator.requestPermission();
+    startDataStreaming();
+  }
+
+  @override
+  void dispose() {
+    cancelSubscriptions();
+    super.dispose();
+  }
+
+  @protected
+  final Logger logger;
+
+  @protected
+  late MqttClient mqttClient;
+
+  @protected
+  void startDataStreaming() => CombineLatestStream(
+        [positionStream(), sensorsStream()],
+        combine,
+      )
+          .where(_isLocationFetched)
+          .where((_) => state.host != null)
+          .where((_) => _isConnected)
+          .delay(const Duration(seconds: 1))
+          .listen(_handleData, onError: _handleError)
+          .addToList(this);
+
+  bool get _isConnected {
+    return mqttClient.connectionStatus?.state == MqttConnectionState.connected;
+  }
+
+  bool _isLocationFetched(List event) {
+    final location = event.elementAt(0) as Location;
+    return location.latitude != null && location.longitude != null;
+  }
+
+  void _handleData(List<dynamic> data) {
+    final location = data.elementAt(0) as Location;
+    final accelerometer = data.elementAt(1) as Accelerometer;
+
+    final agentData = AgentData(
+      userId: _userId,
+      accelerometer: accelerometer,
+      gps: location,
+      timestamp: DateTime.now(),
+    );
+
+    // TODO: analyse road state using custom ML model.
+    final model = MqttModel(roadState: 'small pits', agentData: agentData);
+
+    final MqttClientPayloadBuilder builder = MqttClientPayloadBuilder();
+    builder.addUTF8String(model.toJson().toString());
+
+    mqttClient.publishMessage(_topic, MqttQos.atLeastOnce, builder.payload!);
+  }
+
+  @protected
+  Stream<Accelerometer> sensorsStream() => sensors
+      .accelerometerEventStream(samplingPeriod: SensorInterval.gameInterval)
+      .distinct()
+      .map(_mapToAccelerometerState)
+      .doOnData(_handleAccelerometerData);
+
+  @protected
+  Stream<Location> positionStream() {
     final settings = _configureLocationSettings();
-    Geolocator.getPositionStream(locationSettings: settings)
+    return Geolocator.getPositionStream(locationSettings: settings)
         .distinct()
         .map(_mapToLocationState)
-        .listen(_handleLocationData, onError: _handleError)
-        .addToList(this);
+        .doOnData(_handleLocationData);
+  }
+
+  @protected
+  List<dynamic> combine(Iterable<Object?> values) => [
+        values.elementAt(0),
+        values.elementAt(1),
+      ];
+
+  Future<void> _connect() async {
+    mqttClient = MqttServerClient(state.host ?? _host, '');
+    mqttClient.port = state.port ?? _port;
+
+    mqttClient.logging(on: true);
+
+    final MqttConnectMessage connMess = MqttConnectMessage()
+        .withClientIdentifier('$_userId')
+        .startClean()
+        .keepAliveFor(60)
+        .withWillQos(MqttQos.atLeastOnce);
+
+    mqttClient.connectionMessage = connMess;
+
+    try {
+      await mqttClient.connect();
+    } catch (e, stack) {
+      logger.e('Something went wrong: ', error: e, stackTrace: stack);
+      mqttClient.disconnect();
+    }
   }
 
   AndroidSettings _configureLocationSettings() => AndroidSettings(
@@ -62,24 +170,19 @@ class MetricsControllerAdapter extends MetricsController
     logger.e("$this something went wrong.", error: error, stackTrace: stack);
   }
 
-  @protected
-  final Logger logger;
+  @override
+  Future<void> connect() => _connect();
 
   @override
-  void dispose() {
-    cancelSubscriptions();
-    super.dispose();
+  void disconnect() => mqttClient.disconnect();
+
+  @override
+  void onHostChanged(String text) {
+    state = state.copyWith(host: text);
   }
 
   @override
-  void connect() {
-    // TODO: implement connect
-    throw UnimplementedError();
-  }
-
-  @override
-  void disconnect() {
-    // TODO: implement disconnect
-    throw UnimplementedError();
+  void onPortChanged(String text) {
+    state = state.copyWith(port: int.tryParse(text));
   }
 }
